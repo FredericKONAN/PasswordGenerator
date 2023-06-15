a207197462ea79e965a9b1e6c3e288e6b81ee3d3
FredericKONAN <fredmoye.fk@gmail.com>
2023-06-15 02:17:13 +0000

    form data processing

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -3,15 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PagesController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        dump($request->query->all());
+
         return $this->render("pages/home.html.twig");
     }
 }
